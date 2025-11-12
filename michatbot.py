b1884a6f323faee511f21e1cfd0e42385011396e
Mohamiel <empresaurio.inc@gmail.com>
2025-11-12 10:55:45 -0300

    Update michatbot.py

--- michatbot.py
+++ michatbot.py
@@ -12,7 +12,7 @@ ascii = """
 nombre = st.text_input("Pone tu nombre no wachin, o le tengo que hablar a la pared?")
 if st.button("Despertar ⚰️"):
     
-    st.write(f" {print ascii} {nombre} ")
+    st.write(f" {ascii} {nombre} ")
 
 MODELOS = ["llama-3.1-8b-instant", "llama-3.3-70b-versatile", "deepseek-r1-distill-llama-70b"]
 
@@ -89,6 +89,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
